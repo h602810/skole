d80fe075b0d0d31d26ddde03dd46e28625946777
h602810 <89184881+h602810@users.noreply.github.com>
2022-01-18 10:12:21 +0100

    Delete Uke 34 directory

--- Uke 34/src/no/hvl/dat100/Velkommen.java
+++
@@ -1,7 +0,0 @@
-package no.hvl.dat100;
-
-public class Velkommen {
-    public static void main(String[] args){
-    	System.out.println("**********" + "\n" + "Nå virker det!" + "\n" + "**********");
-    }
-}
